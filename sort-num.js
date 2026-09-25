cee113774468ea6b9a9d0ac15d643949cae7294e
Komalll6 <komalkalra698@gmail.com>
2026-09-26 02:30:50 +0530

    another commit

---
+++ sort-num.js
@@ -0,0 +1,10 @@
+let arr = [23,54,12,90,32,58]
+
+arr.sort((a,b) => a-b)
+
+let min = arr[0]
+let max = arr[arr.length - 1]
+
+console.log(arr)
+console.log("minimum number is: " + min)
+console.log("maximum number is: " + max)
